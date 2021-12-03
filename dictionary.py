53c8a04795eb657bff2a4769aac636280d97802b
Daredvil <95462187+Daredvil@users.noreply.github.com>
2021-12-03 13:05:20 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+# print(len(d))
+
+d={"Although":'In spite of the fact',"Abandon":'Leave',"On tenterhooks":'State of uneasiness',"Impudent":'Rude',"Naive":'Lacking experience in life'}
+
+print("Enter the word you are looking for...")
+word=input()
+print("Meaning of",word,"is-\n",d[word])
